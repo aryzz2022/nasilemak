9eb35c6578830582bbe47cad01c91ac062e39280
aryzz2022 <rosliadamhariz@gmail.com>
2026-08-31 20:59:45 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,13 @@
 <?php
-// db_connect.php - Database connection settings for Nasi Lemak Bob website
+// db_connect.php - Database connection settings for Railway
 
-$db_host = "localhost";
-$db_user = "root";       // change to your MySQL username
-$db_pass = "kptm123";           // change to your MySQL password
-$db_name = "nasi_lemak_bob.";
+$db_host = getenv("MYSQLHOST") ?: "localhost";
+$db_user = getenv("MYSQLUSER") ?: "root";
+$db_pass = getenv("MYSQLPASSWORD") ?: "kptm123";
+$db_name = getenv("MYSQL_DATABASE") ?: "nasi_lemak_bob";
+$db_port = getenv("MYSQLPORT") ?: 3306;
 
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
